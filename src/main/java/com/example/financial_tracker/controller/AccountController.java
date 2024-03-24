bc54f561a1b97361f1b68ebfd540ae704f571ab5
BomScoob <sorawitkraukham@gmail.com>
2024-03-25 00:07:49 +0700

    refactor : remove unused import

--- src/main/java/com/example/financial_tracker/controller/AccountController.java
+++ src/main/java/com/example/financial_tracker/controller/AccountController.java
@@ -3,7 +3,6 @@ package com.example.financial_tracker.controller;
 import com.example.financial_tracker.entities.Account;
 import com.example.financial_tracker.services.AccountServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
